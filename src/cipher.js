97820227b72b006c6a978ee733990f25ffb98b3d
Daniela Collaguazo <dcct84@gmail.com>
2021-09-03 16:17:40 -0400

    cipher working

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,31 @@
 const cipher = {
-  // ...
-};
+  encode: (n,txt) => {
+    n = parseInt(n);
+    txt = txt.toUpperCase();
+    let result = '';
 
+    if (n!==0||txt!==''|| Number.isInteger(n)){      
+      for (let i=0; i < txt.length; i++){
+        let encodedChar = (txt.charCodeAt(i)-65+n)%26;
+        if(encodedChar < 0) encodedChar += 26;
+        result += String.fromCharCode(encodedChar + 65)
+      }
+      }
+      return result;
+    },
+
+  decode: (n,txtToDecode) => {
+    n = parseInt(n);
+    txtToDecode = txtToDecode.toUpperCase();
+    let result = '';
+
+    for (let i=0; i < txtToDecode.length; i++){
+      let decodedChar = (txtToDecode.charCodeAt(i)-65-n)%26;
+      if(decodedChar < 0) decodedChar += 26;
+      result += String.fromCharCode(decodedChar + 65)
+
+    }
+    return result;
+  }
+};  
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
 
+let txtToEncode = document.getElementById('txtToEncode');
+let offset = document.getElementById('txtOffset');
+let txtToDecode = document.getElementById('txtToDecode');
+
+document.getElementById("btnToEncode").addEventListener('click', () => {
+    cipher.encode(offset.value,txtToEncode.value);
+});
+
+document.getElementById('btnToDecode').addEventListener("click", () => {
+    let d = cipher.decode(offset.value, txtToDecode.value);
+    document.getElementById("txtToDecode").value = d;
+});
+
+
+
 console.log(cipher);
